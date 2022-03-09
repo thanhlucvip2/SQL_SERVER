421901303f5aa0cfcb3b28a97f215466276a0ece
thanhlucvip2 <doanthanhluc91bvh@gmail.com>
2022-03-09 20:59:02 +0700

    Buổi 5 - Các phép tính cơ bản trong SQL

---
+++ SQLQuery2.sql
@@ -0,0 +1,13 @@
+﻿-- cộng
+SELECT 1 + 2
+-- trừ
+SELECT 100 - 10
+-- nhân
+SELECT 2*3
+-- chia
+SELECT 3 / 2 --- kết quả là 1 vì đang ở dạng số nguyên (INT)
+SELECT 3.0 / 2  --- kết quả là 1.50000 vì đang ở dạng số thập phân (float)
+SELECT 3/convert(float,2) --- kết quả là 1.5
+SELECT 10 % 4 -- chia lấy dư
+-- hàm số mũ
+SELECT POWER (3,2) -- 3 mũ 2 = 9 
